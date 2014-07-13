7eb31cdd6c44eb35d3ee165a90385f413503d984
Asaf Haddad <asafh@liveperson.com>
2014-07-13 15:55:10 +0300

    Starting HATEOAS explorer

--- src/hateoas.js
+++ src/hateoas.js
@@ -1,13 +1,33 @@
-define(["tiny-emitter","http"],function(Emitter, http) {
+define(["tiny-emitter", "reqwest"], function (Emitter, reqwest) {
 
     function Hateoas(endpoint) {
-        if(this === window) {
+        if (this === window) {
             return new Hateoas(endpoint);
         }
         Emitter.call(this);
 
+        if (endpoint[endpoint.length - 1] === "/") {
+            endpoint = endpoint.substring(0, endpoint.length - 1);
+        }
+
+        function based(path) {
+            if (path[0] !== "/") {
+                path = "/" + path;
+            }
+            return endpoint + path;
+        }
+
+        this.api = reqwest({
+            url: based("/"),
+            type: "json"
+        }).then(function (results) {
+                var ret = {};
+
+                return ret;
+            });
 
     }
+
     Hateoas.prototype = new Emitter();
 
     return Hateoas;
